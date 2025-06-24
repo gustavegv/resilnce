7272d5c144fc53ff176d7fe64f393085d208eb33
Gustav Engvall <gustavengvall@yahoo.se>
2025-06-24 21:47:57 +0200

    fix:adapter falback index.html

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
      adapter: adapter({
-      fallback: '200.html' // or 'index.html'
+      fallback: 'index.html' // or 'index.html'
     }),
     alias: {
       $lib: path.resolve('src/lib'),
